c6ecc1f0e2e45ad3e0633d597a6b3f5e3134740c
gso3kor <GurunathaGGopali.Samartha@in.bosch.com>
2024-11-06 15:35:05 +0530

    Java methods for understanding

---
+++ Bosch-bootcamp-java/src/com/java/labs/SamWorld.java
@@ -0,0 +1,11 @@
+package com.java.labs;
+
+public class SamWorld {
+    public static void main(String[] args){
+        System.out.println("Just do it!!0");
+        /*
+        System.exit(1);
+        System.out.println("After exit");
+         */
+    }
+}
